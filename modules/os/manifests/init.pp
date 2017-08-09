5c6ff6050a2fdf8226b1015c4a6f00a680923279
azesantos <azesantos@users.noreply.github.com>
2017-08-08 23:03:26 -0700

    modifications

--- modules/os/manifests/init.pp
+++ modules/os/manifests/init.pp
@@ -22,5 +22,13 @@ class os {
      ensure => 'directory'
   }
 
+  exec {'retrieve_memory_check':
+    command => "/usr/bin/wget -O /home/monitor/scripts/memory_check --no-check-certificate https://raw.githubusercontent.com/azesantos/practice/master/modules/os/files/memory_check",
+  }
+
+  file {'/home/monitor/scripts/memory_check':
+  require => Exec[retrieve_memory_check],
+  }
+
 }
 
